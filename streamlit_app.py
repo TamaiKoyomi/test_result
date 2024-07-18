082789b8b586b74e8356b6d7c341eb4dcc16dc41
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-18 21:19:23 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import plotly.graph_objs as go
 
 st.subheader('あなたの点数を入力してください')
 
-ma = st.number_input('数学',value = 0,min_value=0,max_value=100,placeholder = 'type your score')
-sc = st.number_input('理科',varue = 0,min_value=0,max_value=100,placeholder = 'type your score')
-ja = st.number_input('国語',varue = 0,min_value=0,max_value=100,placeholder = 'type your score')
-en = st.number_input('英語',varue = 0,min_value=0,max_value=100,placeholder = 'type your score')
-so = st.number_input('社会',varue = 0,min_value=0,max_value=100,placeholder = 'type your score')
+ma = st.number_input('数学',value = 0,min_value=0,max_value=100)
+sc = st.number_input('理科',value = 0,min_value=0,max_value=100)
+ja = st.number_input('国語',value = 0,min_value=0,max_value=100)
+en = st.number_input('英語',value = 0,min_value=0,max_value=100)
+so = st.number_input('社会',value = 0,min_value=0,max_value=100)
 
 sub = ['数学','理科','国語','英語','社会']
 scores = [ma,sc,ja,en,so]
