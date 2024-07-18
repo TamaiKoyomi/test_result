edf4e92df8a27e5273d8abb60d7f396f5df5a7dc
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-18 21:38:28 +0000

    sfgd

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,13 @@ ja = st.number_input('国語',value = 0,min_value=0,max_value=100)
 en = st.number_input('英語',value = 0,min_value=0,max_value=100)
 so = st.number_input('社会',value = 0,min_value=0,max_value=100)
 
+data = go.Bar(
+    x = ['数学','理科','国語','英語','社会'],
+    y = [ma,sc,ja,en,so]
+)
+fig = go.Figure(
+    data = data,
+)
+
 if st.button('結果を見る'):
-    data = go.Bar(
-        x = ['数学','理科','国語','英語','社会'],
-        y = [ma,sc,ja,en,so]
-    )
-    fig = go.Figure(
-        data = data,
-    )
     fig.show()
