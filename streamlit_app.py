728d7fda9da459ab5db9c6e652bb4fa48e1f2bdd
TamaiKoyomi <168719766+TamaiKoyomi@users.noreply.github.com>
2024-07-18 07:35:41 +0000

    adsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.subheader('あなたの点数を入力してください')
 
-ma = st.number_input('数学')
-sc = st.number_input('理科')
-ja = st.number_input('国語')
-en = st.number_input('英語')
-so = st.number_input('社会')
+ma = round(st.number_input('数学'))
+sc = round(st.number_input('理科'))
+ja = round(st.number_input('国語'))
+en = round(st.number_input('英語'))
+so = round(st.number_input('社会'))
